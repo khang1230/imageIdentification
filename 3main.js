0473fcc786774bf4ce2821e9673a4abd1c9a389d
khang1230 <108559508+khang1230@users.noreply.github.com>
2024-02-14 15:33:25 -0800

    Add files via upload

---
+++ 3main.js
@@ -0,0 +1,47 @@
+var objects = []
+var status = ""
+
+function preload() {
+    lego = loadImage("3.jpeg")
+}
+
+function setup() {
+    canvas = createCanvas(700, 500)
+    canvas.center()
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded)
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!")
+    document.getElementById("status").innerHTML = "Status: Detecting Objects"
+    status = true
+    objectDetector.detect(lego, gotResults)
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log("works")
+        console.log(results)
+        //console.log(results[2].label, results[2].width, results[2].height, results[2].x, results[2].y)
+        console.log(results[0].label)
+        objects = results
+    }
+}
+
+function draw() {
+    image(lego, 0, 0, 700, 500)
+    console.log(objects.length)
+    document.getElementById("objectsDetected").innerHTML = "Objects in the Image: "+ objects.length
+    for (var i = 0; i < objects.length; i++) {
+        noFill()
+        stroke("green")
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height)
+    }
+
+}
+
+function back(){
+    window.location = "index.html"
+}
